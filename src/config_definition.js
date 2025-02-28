9c34129a881dd41e8bc9641104ab416909f1fa2c
Alex Abetov <abetovalex93@gmail.com>
2025-02-28 14:54:29 +0300

    Создана функция парсящая выбранный уровень конфигурации в название файла
    Проведены тесты

---
+++ src/config_definition.js
@@ -0,0 +1,31 @@
+/**
+ * Функция по уровню конфигурации, установленном в глобальном файле .env (чаще всего так, но можно откуда угодно взять) определяет, какой будет подгружен конфигурационный файл с константами
+ * @param {string} lvl строка-значение уровня конфигурации
+ */
+const MIN_VALUE = 'min'
+const MIDDLE_VALUE = 'middle'
+const HIGH_VALUE = 'high'
+let values_lvls = [MIN_VALUE, MIDDLE_VALUE, HIGH_VALUE]
+
+function configDefinition(lvl){
+    if (!values_lvls.includes(lvl)){
+        throw new Error(`Установленное значение уровня конфигурации ${lvl} не соответствует требуемому. Список допустимых уровней:
+${values_lvls.map((value, index) => `${index+1}) ${value}`).join(', \n')}`)
+    }
+    switch(lvl){
+        case MIN_VALUE: {
+            return `.${MIN_VALUE}-env`
+        }
+        case MIDDLE_VALUE: {
+            return `.${MIDDLE_VALUE}-env`
+        }
+        case HIGH_VALUE: {
+            return `.${HIGH_VALUE}-env`
+        }
+        default: {
+            throw new Error('Для данного значения ещё не реализован обработчик. Обратитесь к разработчику программы')
+        }
+    }
+}
+
+module.exports = configDefinition
