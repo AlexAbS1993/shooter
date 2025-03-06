e4bf452aa3b3071e0991780fbcc549e57fd140b2
Alex Abetov <abetovalex93@gmail.com>
2025-03-07 02:07:22 +0300

    Планируется переработка DefaultModel

--- src/defaultModel.js
+++ src/defaultModel.js
@@ -5,4 +5,13 @@ class DefaultModel extends AbstractModel{
     constructor(){
         super()
     }
+    getCountOfTables() {
+        return this.tables.length
+      }
+    getTables() {
+        return this.tables
+    }
+    
 }
+
+module.exports = DefaultModel
